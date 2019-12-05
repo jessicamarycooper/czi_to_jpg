b8a677d61c7d344c65d081565fb47b7b484888a1
Jessica Cooper <jmc31@st-andrews.ac.uk>
2019-12-05 16:20:16 +0000

    Added resizing functionality

--- czi_to_jpg.py
+++ czi_to_jpg.py
@@ -11,7 +11,8 @@ import javabridge
 import bioformats
 from PIL import Image
 import argparse
-from os import listdir
+import glob
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--patch_dim', type=int, default=512, help='Patch dimension. Default is 512.)')
@@ -22,27 +23,61 @@ parser.add_argument('--jpg_dir', default='imgs/jpgs', help='Where to save entire
 parser.add_argument('--save_blank', action='store_true', help='Whether or not to save blank patches (i.e. no pixel variation whatsoever, such as at edge of slides)')
 parser.add_argument('--no_patch', action='store_true', help='If you just want a jpg of the czi file without patches, use this. I suggest you set the series value high to avoid creating a giant '
                                                             'monster jpg.')
+parser.add_argument('--resize', default="0,0", help='Optionally provide desired image dimensions separated by a comma, e.g. h,w, which will be used to size the entire slide jpg (with '
+                                                       'rotation '
+                                                                 'and padding if '
+                                                                 'necessary). If not provided, the jpg dimensions will reflect the czi dimensions and series.')
 
 args = parser.parse_args()
+
+
 
 javabridge.start_vm(class_path=bioformats.JARS)
 
 PATCH_DIM = args.patch_dim
 SERIES = args.series
-im_names = listdir(args.czi_dir)
+
+im_names = glob.glob("{}/*.czi".format(args.czi_dir))
 
 
 def normalise(x):
+    print('Normalising pixel values...')
     return (x - x.min()) / (x.max() - x.min())
 
 
-for i in im_names:
+def resize(im, target_height, target_width):
+    print('Image dimensions:', im.size)
+    old_width, old_height = im.size
+    target_orientation = 'P' if target_height > target_width else 'L'
+    old_orientation = 'P' if old_height > old_width else 'L'
 
+    if old_orientation != target_orientation:
+        print('Rotating image...')
+        im = im.rotate(90, expand=True)
+        old_width, old_height = im.size
+
+    print('Resizing image...')
+    if target_height == target_width:
+        ratio = target_width/max(old_width, old_height)
+    else:
+        ratio = min(target_width, target_height)/min(old_width, old_height)
+    new_width = int(old_width * ratio)
+    new_height = int(old_height * ratio)
+    im = im.resize((new_width, new_height), resample=Image.LANCZOS)
+    new_im = Image.new(im.mode, (target_width, target_height))
+    new_im.paste(im, ((target_width - new_width) // 2, (target_height - new_height) // 2))
+    return new_im
+
+
+for i in im_names:
+    i = i.split('/')[-1]
     print(i)
+
     img = normalise(bioformats.load_image('{}/{}'.format(args.czi_dir, i), series=SERIES)) * 255
     x_dim, y_dim = img.shape[0], img.shape[1]
 
     if not args.no_patch:
+        print('Generating patches...')
         for x in range(0, x_dim - PATCH_DIM, PATCH_DIM):
             for y in range(0, y_dim - PATCH_DIM, PATCH_DIM):
                 print(x, y)
@@ -51,8 +86,10 @@ for i in im_names:
                     patch = Image.fromarray(patch.astype('uint8'))
                     patch.save('{}/{}_{}_{}_{}.jpg'.format(args.patch_dir, i, SERIES, x, y))
 
-    whole_img = Image.fromarray(img.astype('uint8'))
-    whole_img.save('{}/{}_{}.jpg'.format(args.jpg_dir, i, SERIES))
+    img = Image.fromarray(img.astype('uint8'))
+    img.save('{}/{}_{}.jpg'.format(args.jpg_dir, i, SERIES))
+    if args.resize != "0,0":
+        img = resize(img, int(args.resize.split(',')[0]), int(args.resize.split(',')[1]))
+    img.save('{}/{}_{}_RESIZED.jpg'.format(args.jpg_dir, i, SERIES))
 
 javabridge.kill_vm()
-
